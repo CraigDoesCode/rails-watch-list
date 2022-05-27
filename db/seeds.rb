b1fea494da70237febbf3a811296ef7dcadac30e
craig norford <craig.a.norford@gmail.com>
2022-05-27 12:54:09 +0100

    gem lock

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+
+movies = JSON.parse(URI.open(url).read)
+Movie.destroy_all
+List.destroy_all
+movies['results'].each do |movie|
+  Movie.create!(title: movie['original_title'],
+                overview: movie['overview'],
+                poster_url: "https://image.tmdb.org/t/p#{movie['poster_path']}",
+                rating: movie['vote_average'].to_f)
+end
+
+List.create!(name: 'horror')
+List.create!(name: 'comedy')
+List.create!(name: 'romantic')
+List.create!(name: 'thriller')
